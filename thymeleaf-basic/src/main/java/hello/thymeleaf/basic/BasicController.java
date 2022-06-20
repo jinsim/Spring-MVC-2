2bdf8762693b7a2a8597b2c13a367d94f85855a7
jinsim <simsime@korea.ac.kr>
2022-06-20 12:30:40 +0900

    블록

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -134,5 +134,9 @@ public class BasicController {
         return "basic/comments";
     }
 
-
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
 }
