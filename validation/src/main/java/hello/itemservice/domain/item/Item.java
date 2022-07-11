0f881451f925c00f7e32bf13a8dff2c990374d0b
jinsim <simsime@korea.ac.kr>
2022-07-11 13:45:48 +0900

    Bean Validation - 시작

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,27 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+// hibernate 구현체에만 동작됨.
+import org.hibernate.validator.constraints.Range;
+// BeanValidation이 표준적으로 제공함. 어떤 구현체에서든 동작함.
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
