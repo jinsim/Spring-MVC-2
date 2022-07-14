1c66eb465cc5d06fe7630c9abadbde55848fbcff
jinsim <simsime@korea.ac.kr>
2022-07-14 11:28:40 +0900

    로그인 처리하기 - 쿠키 사용

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,43 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+    //    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        // 쿠키를 가져오는 것은 HttpServletRequest에서도 가능하지만, 여기선 CookieValue를 사용하겠다.
+        // 가져오는 쿠키 값은 String이지만, 자동으로 타입 컨버팅을 해줘서 Long으로 변경해준다.
+        // 로그인하지 않은(쿠키 값이 없는) 사용자도 들어오기 때문에 required는 false로 둔다.
+
+        if (memberId == null) {
+            return "home";
+        }
+
+        // 로그인 된 사용자
+        Member loginMember = memberRepository.findById(memberId);// 쿠키 값이 정상적으로 회원을 찾을 수 있는지 확인한다.
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome"; // 로그인 사용자 전용 홈을 보여준다.
     }
 }
--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -25,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -36,8 +38,12 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        // 로그인 성공 처리 TODO
+        // 로그인 성공 처리
 
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        // 생성한 쿠키를 서버에서 response에 넣어 보내줘야 한다. HttpServletResponse 추가
+        // 쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료 시 모두 종료)
+        response.addCookie(idCookie);
         return "redirect:/";
     }
 }
