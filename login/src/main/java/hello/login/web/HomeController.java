438dd6371598292dd8f41b3a5eba09c2b43386a6
jinsim <simsime@korea.ac.kr>
2022-07-15 13:37:39 +0900

    로그인 처리하기 - 직접 만든 세션 적용

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,19 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
     //    @GetMapping("/")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
         // 쿠키를 가져오는 것은 HttpServletRequest에서도 가능하지만, 여기선 CookieValue를 사용하겠다.
         // 가져오는 쿠키 값은 String이지만, 자동으로 타입 컨버팅을 해줘서 Long으로 변경해준다.
@@ -40,4 +44,20 @@ public class HomeController {
         model.addAttribute("member", loginMember);
         return "loginHome"; // 로그인 사용자 전용 홈을 보여준다.
     }
+
+    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+
+        // 로그인 판별 = 세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member) sessionManager.getSession(request);
+
+        // 로그인
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+        return "loginHome";
+    }
+
 }
