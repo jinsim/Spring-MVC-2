14e57b8d45e4305d51af23095fe36a0c87f8c8f6
jinsim <simsime@korea.ac.kr>
2022-07-16 15:30:40 +0900

    세션 정보와 타임아웃 설정

---
+++ login/src/main/java/hello/login/web/session/SessionInfoController.java
@@ -0,0 +1,35 @@
+package hello.login.web.session;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+
+@Slf4j
+@RestController
+public class SessionInfoController {
+
+    @GetMapping("/session-info")
+    public String sessionInfo(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "세션이 없습니다.";
+        }
+
+        // 세션 데이터 출력
+        session.getAttributeNames().asIterator()
+                .forEachRemaining(name -> log.info("session name={}, value={}", name, session.getAttribute(name))); // 배열을 하나씩 넣어서 루프를 돌린다.
+
+        // 세션이 기본으로 제공하는 데이터
+        log.info("sessionId={}", session.getId());
+        log.info("getMaxInactiveInterval={}", session.getMaxInactiveInterval());
+        log.info("creationTime={}", new Date(session.getCreationTime())); // 기본이 Long이어서 Date에 넣는다.
+        log.info("lastAccessedTime={}", new Date(session.getLastAccessedTime()));
+        log.info("isNew={}", session.isNew());
+
+        return "세션 출력";
+    }
+}
