4376bee74b05e6b08c6849b5adf834e931356190
jinsim <simsime@korea.ac.kr>
2022-07-21 12:48:33 +0900

    스프링 인터셉터 - 인증 체크

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -3,6 +3,7 @@ package hello.login;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,9 +22,16 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**") // 서블릿이랑 패턴이 다르다. / 하위 전부를 나타냄.
                 .excludePathPatterns("/css/**", "/*.ico", "/error"); // 이 경로는 해당 인터셉터를 호출하지 않는다.
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                // 패턴을 편하고 세밀하게 할 수 있어서 편리하다.
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/member/add", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error");
     }
 
-//    @Bean // 로그는 인터셉터 로그를 사용한다.
+//    @Bean // 로그는 인터셉터를 활용한다.
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter()); // 우리가 만든 로그 필터를 넣는다.
@@ -33,7 +41,7 @@ public class WebConfig implements WebMvcConfigurer {
         return filterRegistrationBean;
     }
 
-    @Bean
+//    @Bean // 인증 체크는 인터셉터를 활용한다.
     public FilterRegistrationBean loginCheckFilter() { // 메모리의 메소드 하나 호출하는 것에 대한 성능 이슈는 없다.
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheckFilter()); // 우리가 만든 로그 필터를 넣는다.
