44d1dc25aa808f04233265f4230d09d2c51e0e29
jinsim <simsime@korea.ac.kr>
2022-08-25 11:19:09 +0900

    스프링에 Converter 적용하기

---
+++ typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -0,0 +1,22 @@
+package hello.typeconverter;
+
+import hello.typeconverter.converter.IntegerToStringConverter;
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    // 스프링 웹 MVC에 등록할 때는 WebMvcConfigurer를 구현한다
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToIntegerConverter());
+        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+    }
+}
